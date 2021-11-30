9eb20d8e48e4211e04d333453efe27325056732a
saku <saku0aab@gmail.com>
2021-12-01 00:11:45 +0900

    ビルドスクリプトを修正

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z "$(git status --porcelain)" ]; then
+  echo "ビルドを開始します。"
+else
+  git status
+  echo "ビルドするためには変更点をコミットする必要があります。"
+  exit 1
+fi
+
 function build_image(){
     imgname=$1
     docker build --tag=$imgname -f Docker/Dockerfile.latest Docker/
